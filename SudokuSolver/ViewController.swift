ccc72fd90f9065cf014185c4e832d253b13c76ba
JiwooL0920 <JiwooL0920@gmail.com>
2019-12-24 16:35:26 -0500

    initial commit

---
+++ SudokuSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Jiwoo Lee on 2019-12-24.
+//  Copyright © 2019 Jiwoo Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
